f04fd1da37211e4d4aed6824f0cd87c039992df0
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-08-23 14:00:15 -0400

    successfully scraping the website and logging to console.

---
+++ models/Posts.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose"); 
+
+var Schema = mongoose.Schema; 
+
+var PostsSchema = new Schema ({
+
+    title: {
+        type: String, 
+        required: true
+    }, 
+
+    link: {
+        type: String, 
+        required: true
+    }, 
+
+    summary: {
+        type: String
+    }
+
+})
+
+var Posts = mongoose.model("Posts", PostsSchema); 
+
+module.exports = Posts; 
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // Adding npm packages  
 var express = require("express"); 
+var mongoose = require("mongoose"); 
+
+// Scrapers 
+var axios = require("axios"); 
+var cheerio = require("cheerio"); 
 
 // Setting up the app and the port 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// Define the db
+var db = require("./models"); 
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: false }));
@@ -11,9 +19,78 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Mongo DB Connection 
-// mongoose.connect("mongodb://localhost/lisbeth", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/lisbeth", { useNewUrlParser: true });
 
 // Listener to start the server
 app.listen(PORT, () => {
     console.log("App running on Port " + PORT); 
 })
+
+// Root Route 
+app.get("/", (req, res) => {
+    res.send("This is the main page"); 
+})
+
+app.get("/scrape", (req, res) => {
+    axios.get("https://wordswithlisbeth.com/blog/").then((response) => {
+        var $ = cheerio.load(response.data)
+
+        $(".entry-wrap").each((i, element) => {
+            // var result = []; 
+
+            // result.title = $(element).find(".entry-title").text();
+            // result.link = $(element).find(".entry-title").children().attr("href");
+            // result.summary = $(element).find(".entry-content").text();
+
+            var title = $(element).find(".entry-title").text();
+            var link = $(element).find(".entry-title").children().attr("href");
+            var summary = $(element).find(".entry-content").text();
+
+            db.Posts.create({
+                title:title, 
+                link:link,  
+                summary:summary
+            }, 
+            (err, insert) => {
+                if (err) {
+                    console.log(err); 
+                }
+                else {
+                    console.log(insert); 
+                }
+            })
+                
+        })
+    })
+})
+
+// Get route to scrape entries from the web Site 
+// app.get("/scrape", (req, res) => {
+//     axios.get("https://wordswithlisbeth.com/blog/").then((response) => {
+//         var $ = cheerio.load(response.data); 
+
+//         $(".entry-wrap").each((i, element) => {
+//             var title = $(element).find(".entry-title").text();
+//             var link = $(element).find(".entry-title").children().attr("href");
+//             var summary = $(element).find(".entry-content").text();
+
+//             if (title && link && summary) {
+//                 db.posts.insert ({
+//                     title: title, 
+//                     link: link,
+//                     summary: summary 
+//                 }, 
+//                 (err, insert) => {
+//                     if (err) {
+//                         console.log(err); 
+//                     } else {
+//                         console.log("database insert successful"); 
+//                     }
+//                 }); 
+//             }
+//         }); 
+//     }); 
+
+//     res.send("Got the articles!!"); 
+
+// })
